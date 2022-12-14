33e43692af009b8f4fc17238ae3c0bc137ea8a23
rangerboy574 <jdeweese574@gmail.com>
2022-12-14 17:11:50 -0600

    Initial Commit

---
+++ CryptoManiac/CryptoManiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoManiacApp.swift
+//  CryptoManiac
+//
+//  Created by Joseph  DeWeese on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoManiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
